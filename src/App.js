8303f424e2d968bab80cc3ea9530767ee8078b5e
shahid787878 <shahidpatel221@gmail.com>
2024-11-22 19:03:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -38,21 +38,28 @@ const App = () => {
     setTime(0);
   };
   return (
-    <div className="stopwatch-container">
-      <p className="stopwatch-time">
+< >
+       <div className="stopwatch-container container-fluid bg-dark btnshop  text-white rounded-5 " style={{height:'100vh'}}>
+     <div className="rounded-5 bg-danger w-50 mx-auto mt-5 pt-5 ">
+ <img className="text-center mx-auto d-flex" style={{height:'30vh', width:'50vh',borderRadius:'50vh'}} src="https://images.pexels.com/photos/9944852/pexels-photo-9944852.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" srcset="" />
+      <h1 className="text-center">Click on Start</h1>
+      <p className="stopwatch-time text-center mx-auto bg-primary fs-1 rounded-5 display-6 w-50">
         {hours}:{minutes.toString().padStart(2, "0")}:
         {seconds.toString().padStart(2, "0")}:
         {milliseconds.toString().padStart(2, "0")}
       </p>
-      <div className="stopwatch-buttons">
-        <button className="stopwatch-button" onClick={startAndStop}>
+      <div className="stopwatch-buttons border mx-auto text-center borderd bntshop rounded-5">
+        <button className="stopwatch-button btn btnshop me-5 " onClick={startAndStop}>
           {isRunning ? "Stop" : "Start"}
         </button>
-        <button className="stopwatch-button" onClick={reset}>
+        <button className="stopwatch-button btnshop btn me-5" onClick={reset}>
           Reset
         </button>
+        </div>
       </div>
-    </div>
+    </div>     
+
+</>
   );
 };
 
